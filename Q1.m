04eeaddc93e62d1d4a88b7210050ac8b54564bd9
Tiarnach O Riada <tiarnachreidy@gmail.com>
2020-11-29 13:05:09 +0000

    Q1 DUN

--- Q1.m
+++ Q1.m
@@ -1,8 +1,8 @@
-PO  = @(chi) 100*exp(-pi*chi./sqrt(1-chi.^2));
-prosp_chi = 0.6:1e-8:0.8;
-[md, mi] = min(abs(PO(prosp_chi)-5));
-chi = prosp_chi(mi)
+PO  = @(zeta) 100*exp(-pi*zeta./sqrt(1-zeta.^2));
+prosp_zeta = 0.6:1e-8:0.8;
+[md, mi] = min(abs(PO(prosp_zeta)-5));
+zeta = prosp_zeta(mi)
 
-chi_chosen = 0.72;
-o_n = 4/(chi_chosen*1);
-o_gc = o_n*sqrt(sqrt(1+4*chi_chosen^4)-2*chi_chosen^2)
+zeta_chosen = 0.72;
+o_n = 4/(zeta_chosen*1);
+o_gc = o_n*sqrt(sqrt(1+4*zeta_chosen^4)-2*zeta_chosen^2)
